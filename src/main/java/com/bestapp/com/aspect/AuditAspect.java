60a8138c64b135112288573e4af38a7410a04b8d
porovoz <falkon1@list.ru>
2025-11-27 13:24:10 +0300

    Some minor corrections.

--- src/main/java/com/bestapp/com/aspect/AuditAspect.java
+++ src/main/java/com/bestapp/com/aspect/AuditAspect.java
@@ -11,24 +11,19 @@ import java.util.Arrays;
 @Aspect
 public class AuditAspect {
 
-    // Defining pointcut for all product-related methods
     @Pointcut("execution(* com.bestapp.com.service.impl.ProductServiceImpl.*(..))")
     public void productServiceMethods() {}
 
-    // Logging user actions before method execution
     @Before("productServiceMethods()")
     public void logBefore(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
-        // Выводим информацию о том, какой метод был вызван и с какими параметрами
         System.out.println("Audit log: User performed action - " + methodName + " with parameters: " + Arrays.toString(args));
     }
 
-    // Method execution logging
     @After("productServiceMethods()")
     public void logAfter(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
-        // Completing action logging
         System.out.println("Audit log: Action " + methodName + " completed successfully.");
     }
 
--- src/main/java/com/bestapp/com/aspect/PerformanceLoggingAspect.java
+++ src/main/java/com/bestapp/com/aspect/PerformanceLoggingAspect.java
@@ -7,25 +7,13 @@ import org.aspectj.lang.annotation.Aspect;
 @Aspect
 public class PerformanceLoggingAspect {
 
-    // Pointcut for all the methods
     @Around("execution(* com.bestapp.com.*.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        // Getting current time before method execution
         long startTime = System.nanoTime();
-
-        // Method execution
         Object result = joinPoint.proceed();
-
-        // Getting current time after method execution
         long endTime = System.nanoTime();
-
-        // Calculating execution time in milliseconds
-        long duration = (endTime - startTime) / 1_000_000; // converting to milliseconds
-
-        // Execution time logging
+        long duration = (endTime - startTime) / 1_000_000;
         System.out.println("Performance log: Method " + joinPoint.getSignature().getName() + " executed in " + duration + " ms");
-
         return result;
     }
 
